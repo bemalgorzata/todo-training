cd4e914087ac05f2dc2644a5967ad5403262ee06
bemalgorzata <bemlgorzata@gmail.com>
2022-03-02 19:05:56 +0100

    strona główna

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
+import { KontaktPageModule } from './pages/kontakt.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'onas', 
+        loadChildren: () => AboutUsPageModule
+      },
+  { 
+        path: 'kontakt', 
+        loadChildren: () => KontaktPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
