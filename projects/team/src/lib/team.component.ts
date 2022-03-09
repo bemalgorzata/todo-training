f70f03e918737a599d0ef43495518c8ff3f1c99f
bemalgorzata <bemlgorzata@gmail.com>
2022-03-09 21:51:48 +0100

    fist lib create

---
+++ projects/team/src/lib/team.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-team',
+  template: `
+    <p>
+      team works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/team/src/lib/team.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { TeamComponent } from './team.component';
+
+
+
+@NgModule({
+  declarations: [
+    TeamComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    TeamComponent
+  ]
+})
+export class TeamModule { }
