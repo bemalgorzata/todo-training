05e1f7401e9e726e4d023dbf896e3f25d5c8065a
bemalgorzata <bemlgorzata@gmail.com>
2022-03-12 14:45:09 +0100

    team-list component  on page

--- src/app/pages/team.page-module.ts
+++ src/app/pages/team.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TeamPage } from './team.page';
+import { TeamListComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/team-list.component-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +10,9 @@ import { TeamPage } from './team.page';
           path: '',
           component: TeamPage,
         }
-      ])],
+      ]),
+  TeamListComponentModule,
+],
   	declarations: [TeamPage],
   	providers: [],
   	exports: [] })
