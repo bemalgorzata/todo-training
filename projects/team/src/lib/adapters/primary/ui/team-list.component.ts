5c5f41ef5329540bbbea055f61a0e6426c279ce7
bemalgorzata <bemlgorzata@gmail.com>
2022-03-12 23:33:21 +0100

    team list better

--- projects/team/src/lib/adapters/primary/ui/team-list.component.ts
+++ projects/team/src/lib/adapters/primary/ui/team-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({ selector: 'lib-team-list', templateUrl: './team-list.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class TeamListComponent {
+    params$ = this._activatedRoute.params;
+    constructor(private _activatedRoute: ActivatedRoute) { }
 
 }
