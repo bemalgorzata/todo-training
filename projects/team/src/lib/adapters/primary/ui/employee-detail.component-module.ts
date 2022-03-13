3506544700dda5ae1fe0a8bbd63b88847ae62586
bemalgorzata <bemlgorzata@gmail.com>
2022-03-13 13:13:37 +0100

    employee-detail + routing

--- projects/team/src/lib/adapters/primary/ui/employee-detail.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/employee-detail.component-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeDetailComponent } from './employee-detail.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [EmployeeDetailComponent],
-  	providers: [],
-  	exports: [EmployeeDetailComponent] })
+@NgModule({
+	imports: [CommonModule, RouterModule],
+	declarations: [EmployeeDetailComponent],
+	providers: [],
+	exports: [EmployeeDetailComponent]
+})
 export class EmployeeDetailComponentModule {
 }
--- projects/team/src/public-api.ts
+++ projects/team/src/public-api.ts
@@ -4,3 +4,4 @@ import { from } from "rxjs";
  * Public API Surface of team
  */
 export * from './lib/adapters/primary/ui/team-list.component-module';
+export * from './lib/adapters/primary/ui/employee-detail.component-module'
