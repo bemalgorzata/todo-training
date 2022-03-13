1a65f3de5f566d54d82f49ea0629371ff9d78dfa
bemalgorzata <bemlgorzata@gmail.com>
2022-03-13 22:38:39 +0100

    complete team-list DTO +and  ngFor

--- projects/team/src/lib/adapters/primary/ui/team-list.component.ts
+++ projects/team/src/lib/adapters/primary/ui/team-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { EmployeeDTO } from '../../../application/ports/secondary/employee.dto';
 
 @Component({
@@ -9,21 +10,27 @@ import { EmployeeDTO } from '../../../application/ports/secondary/employee.dto';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TeamListComponent {
-    employee1: EmployeeDTO = {
-        name: "Andrzej",
-        image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.6435-9/38026346_1754868611276419_1625273276152414208_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=174925&_nc_ohc=HEHMrUj36dEAX9MvMrn&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT8S-DEBV_ABjq9HxVsWdaJSfn8z4vX2yBWDvZiuez7gAg&oe=624ABB41",
-        info: "Andrzej lubi zjeżdzać na nartach."
-
-    }
-    employee2: EmployeeDTO = {
-        name: "Adam",
-        image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.6435-9/161738566_287508819404472_9085678602602917289_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=l4N-xvpkXUsAX_Pz0cP&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT83h42ImNBUFvsnlY2nTric3LnLCzbUYaYi0ycNrc7-0Q&oe=624D3119",
-        info: "Adam lubi skakać na nartach."
-    }
-    employee3: EmployeeDTO = {
-        name: "Justyna",
-        image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.18169-9/15181127_1663000840410932_2209517998189993037_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_ohc=cDTPsYC41uMAX9dWEO9&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT_mwXPJyShBkqHf15VwqaloDqHsUW2d2uYqHJxZIAwsgg&oe=624CBE33",
-        info: "Justyna lubi biegać na nartch."
-    }
-
+    employees$: Observable<EmployeeDTO[]> = of([
+        {
+            name: "Andrzej",
+            image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.6435-9/38026346_1754868611276419_1625273276152414208_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=174925&_nc_ohc=HEHMrUj36dEAX9MvMrn&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT8S-DEBV_ABjq9HxVsWdaJSfn8z4vX2yBWDvZiuez7gAg&oe=624ABB41",
+            info: "Andrzej lubi zjeżdzać na nartach.",
+            link: "/pracownik/Andrzej",
+            button: "Poznaj Andrzeja"
+        },
+        {
+            name: "Adam",
+            image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.6435-9/161738566_287508819404472_9085678602602917289_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=l4N-xvpkXUsAX_Pz0cP&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT83h42ImNBUFvsnlY2nTric3LnLCzbUYaYi0ycNrc7-0Q&oe=624D3119",
+            info: "Adam lubi skakać na nartach.",
+            link: "/pracownik/Adam",
+            button: "Poznaj Adama"
+        },
+        {
+            name: "Justyna",
+            image: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.18169-9/15181127_1663000840410932_2209517998189993037_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_ohc=cDTPsYC41uMAX9dWEO9&_nc_ht=scontent.fpoz3-1.fna&oh=00_AT_mwXPJyShBkqHf15VwqaloDqHsUW2d2uYqHJxZIAwsgg&oe=624CBE33",
+            info: "Justyna lubi biegać na nartch.",
+            link: "/pracownik/Justyna",
+            button: "Poznaj Justynę"
+        }
+    ])
 }
--- projects/team/src/lib/application/ports/secondary/employee.dto.ts
+++ projects/team/src/lib/application/ports/secondary/employee.dto.ts
@@ -2,4 +2,6 @@ export interface EmployeeDTO {
   readonly name: string;
   readonly image: string;
   readonly info: string;
+  readonly link: string;
+  readonly button: string
 }
