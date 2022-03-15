aeb2db9f3ea8f26212d254a9adcb59958a3a3569
bemalgorzata <bemlgorzata@gmail.com>
2022-03-15 21:54:13 +0100

    add lib-team to eployees page

--- src/app/pages/employees.page-module.ts
+++ src/app/pages/employees.page-module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { EmployeesPage } from './employees.page';
+import { EmployeeDetailComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/employee-detail.component-module';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: EmployeesPage,
-        }
-      ])],
-  	declarations: [EmployeesPage],
-  	providers: [],
-  	exports: [] })
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild([
+    {
+      path: '',
+      component: EmployeesPage,
+    }
+  ]),
+    EmployeeDetailComponentModule
+  ],
+  declarations: [EmployeesPage],
+  providers: [],
+  exports: []
+})
 export class EmployeesPageModule {
 }
