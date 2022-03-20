ebf60f60b37e111fd31b7d081537808b903e647a
bemalgorzata <bemlgorzata@gmail.com>
2022-03-20 21:58:09 +0100

    firebase empl service used in page

--- projects/team/src/public-api.ts
+++ projects/team/src/public-api.ts
@@ -6,3 +6,4 @@ import { from } from "rxjs";
 export * from './lib/adapters/primary/ui/team-list.component-module';
 export * from './lib/adapters/primary/ui/employee-detail.component-module'
 export * from './lib/adapters/secondary/infrastructure/firebase-employees.service-module';
+export * from './lib/adapters/primary/ui/test.component-module';
--- src/app/pages/about-us.page-module.ts
+++ src/app/pages/about-us.page-module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AboutUsPage } from './about-us.page';
+import { TestComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/test.component-module';
+import { FirebaseEmployeesServiceModule } from '../../../projects/team/src/lib/adapters/secondary/infrastructure/firebase-employees.service-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +11,10 @@ import { AboutUsPage } from './about-us.page';
           path: '',
           component: AboutUsPage,
         }
-      ])],
+      ]),
+  TestComponentModule,
+  FirebaseEmployeesServiceModule
+],
   	declarations: [AboutUsPage],
   	providers: [],
   	exports: [] })
