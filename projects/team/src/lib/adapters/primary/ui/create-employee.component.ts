c6d51d352a6500781a8748f594bc181472ec51fa
bemalgorzata <bemlgorzata@gmail.com>
2022-03-23 17:14:12 +0100

    createEmployee form added and working

--- projects/team/src/lib/adapters/primary/ui/create-employee.component.ts
+++ projects/team/src/lib/adapters/primary/ui/create-employee.component.ts
@@ -1,10 +1,41 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { ADDS_EMPLOYEE_DTO, AddsEmployeeDtoPort } from '../../../application/ports/secondary/adds-employee.dto-port';
 
 @Component({
-    selector: 'lib-create-employee',
-    templateUrl: './create-employee.component.html',
-    encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'lib-create-employee',
+  templateUrl: './create-employee.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateEmployeeComponent {
+  readonly createEmployee: FormGroup = new FormGroup(
+    {
+      name: new FormControl(),
+      info: new FormControl(),
+      imageUrl: new FormControl(),
+      id: new FormControl(),
+      department: new FormControl(),
+      // employeeCount: new FormControl(),
+      buttonInfo: new FormControl()
+    });
+
+  constructor(@Inject(ADDS_EMPLOYEE_DTO) private _addsEmployeeDto: AddsEmployeeDtoPort) {
+  }
+
+  onCreateEmployeeSubmited(createEmployee: FormGroup): void {
+    if (createEmployee.invalid) {
+      return;
+    }
+    this._addsEmployeeDto.add({
+      name: createEmployee.get("name").value,
+      info: createEmployee.get("info").value,
+      imageUrl: createEmployee.get("imageUrl").value,
+      id: createEmployee.get("id").value,
+      department: createEmployee.get("department").value,
+      // employeeCount: createEmployee.get("employeeCount").value,
+      buttonInfo: createEmployee.get("department").value
+
+    });
+  }
 }
