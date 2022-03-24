5c23f399a6ea59c121cdc4c16125830e50a43ef9
bemalgorzata <bemlgorzata@gmail.com>
2022-03-24 11:33:04 +0100

    DTO Port implemnted

--- projects/messages/src/lib/adapters/secondary/infrastructure/firebase-message.service-module.ts
+++ projects/messages/src/lib/adapters/secondary/infrastructure/firebase-message.service-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirebaseMessageService } from './firebase-message.service';
+import { ADDS_MESSAGE_DTO } from '../../../application/ports/secondary/adds-message.dto-port';
 
-@NgModule({ imports: [AngularFirestoreModule],
-  	declarations: [],
-  	providers: [FirebaseMessageService],
-  	exports: [] })
+@NgModule({
+	imports: [AngularFirestoreModule],
+	declarations: [],
+	providers: [FirebaseMessageService, { provide: ADDS_MESSAGE_DTO, useExisting: FirebaseMessageService }],
+	exports: []
+})
 export class FirebaseMessageServiceModule {
 }
